cc4f53af7e1e492adf5da2f1c89f222a5bab4f71
Arman Ziaei <armanziaei83@gmail.comi>
2020-10-09 16:58:58 -0400

    Feats Java Files Code : Some coded added to mainActivity.java to Run fragments

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void ChangeFragment(View view) {
+        Fragment fragment;
+
+        if (view == findViewById(R.id.button1)) {
+            fragment = new FirstFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction ft = manager.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+
+        }
+        if (view == findViewById(R.id.button2)) {
+            fragment = new SecondFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction ft = manager.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+    }
+    
 }
